2d6f28e4ff50ff85abb8e741f2260f195a8f45f2
Julian Ruiz Diaz <86427486+RDJulian@users.noreply.github.com>
2024-04-17 14:56:51 -0300

    fix: Comparación en ciclo for.

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main() {
     std::vector<int> vector = {3, 5, 7, 10, 9, 2, 1};
     selection<int>::sort(vector);
     std::cout << "Vector ordenado: ";
-    for (auto iter = vector.begin(); iter < vector.end(); iter++) {
+    for (auto iter = vector.begin(); iter != vector.end(); iter++) {
         if (iter == vector.end() - 1) {
             std::cout << *iter << "." << std::endl;
         } else {
